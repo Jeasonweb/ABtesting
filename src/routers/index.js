3652c6c4d10a07bb82c6eac1c0433d2c6df72bd5
brucecham <brucecham@qq.com>
2017-07-06 15:32:36 +0800

    add files

---
+++ src/routers/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+  Route,
+  Redirect,
+  Switch,
+  HashRouter
+} from 'react-router-dom'
+
+import CounterContainer from '@/containers/CounterContainer'
+import AppContainer from '@/containers/AppContainer'
+import HomeContainer from '@/containers/HomeContainer'
+import NoMatchContainer from '@/containers/NoMatchContainer'
+import FormContainer from '@/containers/SimpleFormContainer'
+const routes = (
+  <HashRouter>
+    <AppContainer>
+      <Switch>
+        <Route path='/counter' component={CounterContainer} />
+        <Route path='/home' component={HomeContainer} />
+        <Route path='/form' component={FormContainer} />
+        <Route component={NoMatchContainer} />
+      </Switch>
+      <Redirect from='/' to='/home' />
+    </AppContainer>
+  </HashRouter>
+)
+
+export default routes
